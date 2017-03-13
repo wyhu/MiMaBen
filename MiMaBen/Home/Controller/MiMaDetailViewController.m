82531457d6f2f70503531321ca1d22fd9b6b8f28
胡伟亚 <596031524@qq.com>
2017-03-13 12:01:18 +0800

    斯蒂芬

--- MiMaBen/Home/Controller/MiMaDetailViewController.m
+++ MiMaBen/Home/Controller/MiMaDetailViewController.m
@@ -39,9 +39,7 @@
     self.count.enabled = NO;
     [self initNav];
     
-    //这里是在分支做了一个修改
-    //修改
-    
+    //大家好
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFC) name:UITextFieldTextDidChangeNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFC) name:UITextViewTextDidChangeNotification object:nil];
