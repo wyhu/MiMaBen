6696d5c735a8e75eb4c9e85fe6b17daa160568ed
胡伟亚 <596031524@qq.com>
2017-04-17 09:59:20 +0800

    df add

---
+++ MiMaBen/Setting/View/self.h
@@ -0,0 +1,13 @@
+//
+//  self.h
+//  MiMaBen
+//
+//  Created by huweiya on 2017/4/17.
+//  Copyright © 2017年 5i5j. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface self : UIView
+
+@end
---
+++ MiMaBen/Setting/View/self.m
@@ -0,0 +1,21 @@
+//
+//  self.m
+//  MiMaBen
+//
+//  Created by huweiya on 2017/4/17.
+//  Copyright © 2017年 5i5j. All rights reserved.
+//
+
+#import "self.h"
+
+@implementation self
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
